e5911d32a3014369ed92c9b05fcb79112f559990
DevarimKhayil <wachirabriangeita@gmail.com>
2024-08-13 20:35:26 +0300

    script edit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,59 +1,50 @@
 #!/usr/bin/python3
+"""
+log parsing
+"""
 
 import sys
-import signal
 import re
 
-# Initialize variables
-total_file_size = 0
-status_codes = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
-line_count = 0
 
-# Regular expression pattern to match the expected log format
-log_pattern = re.compile(r'\S+ - \[\S+\] "GET /projects/260 HTTP/1\.1" (\d{3}) (\d+)')
+def output(log: dict) -> None:
+    """
+    helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
-def print_stats():
-    """Prints the accumulated statistics"""
-    print(f"File size: {total_file_size}")
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print(f"{code}: {status_codes[code]}")
 
-def signal_handler(sig, frame):
-    """Handles the SIGINT signal (CTRL + C)"""
-    print_stats()
-    sys.exit(0)
+if __name__ == "__main__":
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
 
-# Register the signal handler for SIGINT
-signal.signal(signal.SIGINT, signal_handler)
+    line_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
 
-# Read from stdin line by line
-try:
-    for line in sys.stdin:
-        match = log_pattern.match(line)
-        if match:
-            status_code, file_size = match.groups()
-            total_file_size += int(file_size)
-            if status_code in status_codes:
-                status_codes[status_code] += 1
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if (match):
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
 
-        line_count += 1
+                # File size
+                log["file_size"] += file_size
 
-        if line_count % 10 == 0:
-            print_stats()
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
 
-except Exception as e:
-    pass
-
-# Print remaining stats after the loop ends (in case of incomplete batch)
-print_stats()
-
+                if (line_count % 10 == 0):
+                    output(log)
+    finally:
+        output(log)
